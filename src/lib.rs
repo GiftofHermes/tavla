705af541f19a4a7051f7d5f93176590ad22da98a
GiftofHermes <mimaroglualparslan@gmail.com>
2024-07-28 00:48:45 +0200

    create board :

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod game;
+use game::*;
 
 #[cfg(test)]
 mod tests {
@@ -8,7 +7,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let tavla = Game::new();
+        assert!(tavla);
     }
 }
